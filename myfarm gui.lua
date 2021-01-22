e3f682771b4eff6e3e3bdcc3740ebd5590e12284
watashiwarimurux1 <62368772+watashiwarimurux1@users.noreply.github.com>
2021-01-22 17:27:55 +0300

    MyFarm
    Game ID: 4347362871

--- myfarm gui.lua
+++ myfarm gui.lua
@@ -10,7 +10,7 @@ end)
 w1:Button('Z = FREE ITEM', function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/watashiwarimurux1/myscripts/master/myfarm_free-item-script_obfuscated.lua",true))()
 end)
-w1:Label('You need to inject each')
+w1:Label('You need to click & execute each')
 w1:Label('for hotkeys to work')
 w1:Label('Coin-Free item little buggy')
 
